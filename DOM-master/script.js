9cbb68ae863ad9d68c4875615220b04b05609dd7
Viktoriia Mikhailova <mikhailv@arcada.fi>
2025-09-24 10:20:49 +0300

    effect On and Off are ready

--- DOM-master/script.js
+++ DOM-master/script.js
@@ -1,5 +1,23 @@
 window.onload = function() {
     // Lyssna efter händelser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
 var editTitle = function(e) {
     // Ändra "team-title"
@@ -7,8 +25,22 @@ var editTitle = function(e) {
 
 function effectOn(id){
     // Rita en ram runt personen, kanske lite drop shadow?
+
+    let img = document.getElementById(id);
+    if (img) {
+        img.style.boxShadow = "0 0 15px 5px rgba(0, 0, 0, 0.5)";
+        img.style.border = "2px solid #007bff";
+        img.style.borderRadius = "5px";
+    }
 }
+
 
 function effectOff(id){
     // Stäng av effekten när musen inte längre är ovanför personen
+    let img = document.getElementById(id);
+    if (img) {
+        img.style.boxShadow = "none";
+        img.style.border = "none";
+    }
 }
+
